14b6ede29e6db428b5ca0e60f602a69895ceb8f2
Gabriel <gaabrielramires@gmail.com>
2024-02-20 10:48:56 -0300

    Commit aula 14 - string.format

--- aula14.py
+++ aula14.py
@@ -1,7 +1,9 @@
-a = 'A'
-b = 'B'
+a = 'AAAAA'
+b = 'BBBBBB'
 c = 1.1
-formato = 'a={} b={} c={}'.format(a, b, c)
+string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
+formato = string.format(
+    nome1=a, nome2=b, nome3=c
+)
 
 print(formato)
-print(a)
