0a8c1d06ab7943f72b8df0abc106b1bb1adae8be
Gabriel <gaabrielramires@gmail.com>
2024-02-21 09:55:12 -0300

    Aula 16 - introdução ao if, elif e else

---
+++ aula16.py
@@ -0,0 +1,10 @@
+entrada = input('Você quer entrar ou sair?')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+    
+    print(1515151)
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não digitou nem entrar nem sair')
