4b270fed71f608f5af5f8f5a0bfd3f59c56725eb
Gabriel <gaabrielramires@gmail.com>
2024-02-21 00:18:36 -0300

    Aula 15 - introdução ao input

---
+++ aula15.py
@@ -0,0 +1,7 @@
+# nome = input('Qual o seu nome?')
+
+
+numero_1 = (input('Digite um número: ')) #nesse caso, o input será sempre STR.
+numero_2 = (input('Digite um número: ')) #por ser str, o print abaixo exibirá a concatenação dos nuúmeros digitados
+# nas próximas aulas será apresentado as validações para fazermos a coerção de forma correta
+print(f'A soma dos números é {numero_1 + numero_2}')
