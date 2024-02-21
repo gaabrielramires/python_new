46b0864f89cccb33900379872870a18a8985d7a2
Gabriel <gaabrielramires@gmail.com>
2024-02-21 10:11:36 -0300

    Aprofundamento if, elif e else

---
+++ aula17.py
@@ -0,0 +1,21 @@
+condicao1 = True
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Esse é o código da condição 1')
+elif condicao2:
+    print('Esse é o código da condição 2')
+elif condicao3:
+    print('Esse é o código da condição 3')
+elif condicao4:
+    print('Esse é o código da condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
+
