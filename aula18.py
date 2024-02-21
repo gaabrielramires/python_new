2c3c3694c2764e1aa7a24b5991af766795778729
Gabriel <gaabrielramires@gmail.com>
2024-02-21 10:18:51 -0300

    Aula 18 - introdução ao debugger

---
+++ aula18.py
@@ -0,0 +1,20 @@
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Esse é o código da condição 1')
+elif condicao2:
+    print('Esse é o código da condição 2')
+elif condicao3:
+    print('Esse é o código da condição 3')
+elif condicao4:
+    print('Esse é o código da condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
